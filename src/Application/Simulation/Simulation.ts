2ad4ea53f1008f15fa05993cd110339f36b4f0a8
olivvein <parcouru_epoque.9b@icloud.com>
2025-11-07 02:41:12 +0100

    fleche direction

--- src/Application/Simulation/Simulation.ts
+++ src/Application/Simulation/Simulation.ts
@@ -15,7 +15,7 @@ import {
   model3HighRes,
   model3LowRes
 } from "./Vehicle/Car";
-import { DEFAULT_KEYS_1 } from "./Vehicle/CarControlKeys";
+import { DEFAULT_KEYS_1, DEFAULT_KEYS_2 } from "./Vehicle/CarControlKeys";
  
 
 import { observe } from "mobx";
@@ -110,7 +110,7 @@ async function createCar(config: CarConfig, scene: THREE.Scene) {
   appStore.setInitState(InitState.LOADING);
   car = await createVehicle(
     initCarPosition,
-    DEFAULT_KEYS_1,
+    DEFAULT_KEYS_2,
     world,
     scene,
     rootStore.carStore,
